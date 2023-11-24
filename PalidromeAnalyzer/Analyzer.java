2b598e72d3a70caa568bd4d494a12b90effab086
Alexandros Papafragakis <alexpapafragakis7@gmail.com>
2023-11-24 16:35:41 +0200

    Add files via upload

---
+++ PalidromeAnalyzer/Analyzer.java
@@ -0,0 +1,46 @@
+package A13;
+
+import A12.Ask2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ask3{
+
+    static void File_R(){
+        try {
+            FileInputStream fis = new FileInputStream("Resources/gr.dic.txt");
+            String temp;
+            int count = 0;
+            int sum = 0;
+            int pal = 0;
+            Scanner sc = new Scanner(fis);
+            while(sc.hasNextLine()) {
+                temp = sc.nextLine();
+                count++;
+                sum = sum + temp.length();
+                if(Ask2.isPalindromikiFrash(temp)){
+                    pal++;
+                }
+            }
+            System.out.println("Πλήθος λέξεων: " + count );
+            System.out.println("Μέσο πλήθος λέξεων: " + String.format("%.4f",(double)sum/count));
+            System.out.println("Παλινδρομικές λέξεις: " + pal);
+            System.out.println("Ποσοστό παλινδρομικών λέξεων: " + String.format("%.4f",((double)pal/count)*100));
+
+            sc.close();
+        } catch(IOException e)   {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args){
+        long start = System.currentTimeMillis();
+        File_R();
+        long end = System.currentTimeMillis();
+        float sec = (end - start) / 1000F;
+        System.out.println("Time: " + sec + " seconds");
+    }
+}
+
---
+++ PalidromeAnalyzer/README.md
@@ -0,0 +1,41 @@
+# Palindrome Word Analyzer
+
+This Java program reads words from a file, analyzes them, and provides statistics on palindromic words.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Functionality](#functionality)
+- [Dependencies](#dependencies)
+- [File Format](#file-format)
+
+## Usage
+
+1. **Compile and Run:** Compile the program and run it with the following command:
+
+    ```bash
+    javac Ask3.java
+    java Ask3
+    ```
+
+2. **File Input:** The program reads words from the file `Resources/gr.dic.txt`.
+
+3. **Output:** The program outputs statistics on the words, including the total number of words, the average word length, the number of palindromic words, and the percentage of palindromic words.
+
+## Functionality
+
+The program performs the following tasks:
+   - Reads words from the specified file.
+   - Calculates and outputs the total number of words.
+   - Calculates and outputs the average word length.
+   - Identifies palindromic words and counts their occurrences.
+   - Calculates and outputs the number and percentage of palindromic words.
+
+## Dependencies
+
+- Java Development Kit (JDK)
+
+## File Format
+
+The program expects a file named `gr.dic.txt` in the `Resources` directory. Each line in the file should contain a single word.
+
